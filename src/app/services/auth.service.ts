9a9f6ae115bb131d805f643a46f37d6d347cc43b
Nubla2 <105323283+Nubla2@users.noreply.github.com>
2024-08-02 12:16:33 +0200

    Pagina admin con los elementos para probar el back

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,29 +1,4 @@
-// src/app/services/auth.service.ts
 
-/*import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthService {
-
-  private apiUrl = 'http://localhost:3001'; // URL del servidor Node.js
-
-  constructor(private http: HttpClient) { }
-
-  /*register(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/register`, { email, password });
-  }*/
-   /* register(email: string, password: string): Observable<any> {
-      const user = { email, password };
-      return this.http.post(`${this.apiUrl}/register`, user);
-    }
-  login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
-  }
-}*/
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
@@ -50,3 +25,29 @@ export class AuthService {
 
 }
 
+// src/app/services/auth.service.ts
+
+/*import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:3001'; // URL del servidor Node.js
+
+  constructor(private http: HttpClient) { }
+
+  /*register(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/register`, { email, password });
+  }*/
+   /* register(email: string, password: string): Observable<any> {
+      const user = { email, password };
+      return this.http.post(`${this.apiUrl}/register`, user);
+    }
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/login`, { email, password });
+  }
+}*/
