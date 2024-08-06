fbdb701a3122ee1385288bca1bf818675a534a20
Nubla2 <105323283+Nubla2@users.noreply.github.com>
2024-08-06 14:02:02 +0200

    agenda desplegable

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
@@ -6,12 +7,29 @@ import { MatIconModule } from '@angular/material/icon';
   selector: 'app-home-page',
   standalone: true,
   imports: [
+    CommonModule,
     MatIconModule,
-    MatButtonModule,
+    MatButtonModule
   ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  events = [
+    { isExpanded: false, description: 'Lorem ipsum dolor sit amet', details: 'Descripción de este evento de la agenda que tiene que venir de la BBDD' },
+    { isExpanded: false, description: 'Lorem ipsum dolor sit amet', details: 'Descripción de este evento de la agenda que tiene que venir de la BBDD' },
+    { isExpanded: false, description: 'Lorem ipsum dolor sit amet', details: 'Descripción de este evento de la agenda que tiene que venir de la BBDD' },
+    { isExpanded: false, description: 'Lorem ipsum dolor sit amet', details: 'Descripción de este evento de la agenda que tiene que venir de la BBDD' }
+  ];
 
+  toggleContent(index: number): void {
+    // Cierra todos los eventos
+    this.events.forEach((event, i) => {
+      if (i !== index) {
+        event.isExpanded = false;
+      }
+    });
+    // Alterna el estado del evento clicado
+    this.events[index].isExpanded = !this.events[index].isExpanded;
+  }
 }
